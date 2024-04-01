d6cebee2f4448892df977d83d8ac75a1c079f516
居戎氏 <chen.sst@gmail.com>
2024-04-02 00:59:38 +0800

    在並發測試中只調用一次 RimeApi::setup

--- src/rime_levers.rs
+++ src/rime_levers.rs
@@ -1,6 +1,25 @@
-use anyhow::bail;
-use rime::{rime_api_call, rime_module_call, rime_struct_new, RimeConfig, RimeLeversApi};
+use anyhow::{anyhow, bail};
+use rime::{
+    rime_api_call, rime_module_call, rime_struct_new, RimeConfig, RimeLeversApi, RimeTraits,
+};
 use std::ffi::CString;
+use std::path::PathBuf;
+
+pub fn 設置引擎啓動參數(工作場地: &PathBuf) -> anyhow::Result<()> {
+    std::fs::create_dir_all(工作場地)?;
+    let 場地〇 = CString::new(工作場地.to_str().ok_or(anyhow!("路徑編碼轉換錯誤"))?)?;
+    let 品名〇 = CString::new(env!("CARGO_PKG_NAME"))?;
+    let 版本〇 = CString::new(env!("CARGO_PKG_VERSION"))?;
+    let mut 啓動參數: RimeTraits = rime_struct_new!();
+    啓動參數.data_size = std::mem::size_of::<RimeTraits>() as std::ffi::c_int;
+    啓動參數.shared_data_dir = 場地〇.as_ptr();
+    啓動參數.user_data_dir = 場地〇.as_ptr();
+    啓動參數.distribution_name = 品名〇.as_ptr();
+    啓動參數.distribution_code_name = 品名〇.as_ptr();
+    啓動參數.distribution_version = 版本〇.as_ptr();
+    rime_api_call!(setup, &mut 啓動參數);
+    Ok(())
+}
 
 pub fn 配置補丁(目標配置: &str, 紐: &str, 值: &str) -> anyhow::Result<()> {
     log::debug!("配置補丁: {目標配置}:/{紐} = {值}");
@@ -53,35 +72,29 @@ pub fn 配置補丁(目標配置: &str, 紐: &str, 值: &str) -> anyhow::Result<
 mod tests {
     use super::*;
 
-    use anyhow::anyhow;
     use claims::assert_ok;
-    use rime::RimeTraits;
-    use std::ffi::{c_int, CStr};
+    use lazy_static::lazy_static;
     use std::fs::{read_to_string, remove_file};
+    use std::sync::Once;
 
-    fn rime_setup() -> anyhow::Result<()> {
-        let temp_path_str = CString::new(
-            std::env::temp_dir()
-                .to_str()
-                .ok_or(anyhow!("invaid utf-8 path"))?,
-        )?;
-        let mut test_traits: RimeTraits = rime_struct_new!();
-        test_traits.data_size = std::mem::size_of::<RimeTraits>() as c_int;
-        test_traits.shared_data_dir = temp_path_str.as_ptr();
-        test_traits.user_data_dir = temp_path_str.as_ptr();
-        test_traits.distribution_name = CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
-        test_traits.distribution_code_name = CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
-        test_traits.distribution_version = CStr::from_bytes_with_nul(b"0\0").unwrap().as_ptr();
-        rime_api_call!(setup, &mut test_traits);
-        Ok(())
+    lazy_static! {
+        static ref 測試場地: PathBuf = std::env::temp_dir().join("rime_levers_tests");
+    }
+
+    static 預備本場測試: Once = Once::new();
+
+    fn 預備() {
+        預備本場測試.call_once(|| {
+            assert_ok!(設置引擎啓動參數(&測試場地));
+        })
     }
 
     #[test]
     fn 測試配置補丁_全局配置() {
-        assert_ok!(rime_setup());
+        預備();
         assert_ok!(配置補丁("default", "menu/page_size", "5"));
 
-        let 結果文件 = std::env::temp_dir().join("default.custom.yaml");
+        let 結果文件 = 測試場地.join("default.custom.yaml");
         let 補丁文件內容 = assert_ok!(read_to_string(&結果文件));
         assert!(補丁文件內容.contains(
             r#"
@@ -93,10 +106,10 @@ patch:
 
     #[test]
     fn 測試配置補丁_輸入方案() {
-        assert_ok!(rime_setup());
+        預備();
         assert_ok!(配置補丁("ohmyrime.schema", "menu/page_size", "9"));
 
-        let 結果文件 = std::env::temp_dir().join("ohmyrime.custom.yaml");
+        let 結果文件 = 測試場地.join("ohmyrime.custom.yaml");
         let 補丁文件內容 = assert_ok!(read_to_string(&結果文件));
         assert!(補丁文件內容.contains(
             r#"
@@ -108,14 +121,14 @@ patch:
 
     #[test]
     fn 測試配置補丁_列表值() {
-        assert_ok!(rime_setup());
+        預備();
         assert_ok!(配置補丁(
             "test_patch_list",
             "starcraft/races",
             r#"[protoss, terran, zerg]"#
         ));
 
-        let 結果文件 = std::env::temp_dir().join("test_patch_list.custom.yaml");
+        let 結果文件 = 測試場地.join("test_patch_list.custom.yaml");
         let 補丁文件內容 = assert_ok!(read_to_string(&結果文件));
         println!("補丁文件內容: {}", 補丁文件內容);
         assert!(補丁文件內容.contains(
@@ -131,14 +144,14 @@ patch:
 
     #[test]
     fn 測試配置補丁_字典值() {
-        assert_ok!(rime_setup());
+        預備();
         assert_ok!(配置補丁(
             "test_patch_map",
             "starcraft/workers",
             r#"{protoss: probe, terran: scv, zerg: drone}"#
         ));
 
-        let 結果文件 = std::env::temp_dir().join("test_patch_map.custom.yaml");
+        let 結果文件 = 測試場地.join("test_patch_map.custom.yaml");
         let 補丁文件內容 = assert_ok!(read_to_string(&結果文件));
         assert!(補丁文件內容.contains(
             r#"
