9aa216b7d033223ed3c373ce8aa602cec6df4edd
居戎氏 <chen.sst@gmail.com>
2024-04-17 20:43:07 +0800

    下載配方包

--- src/download.rs
+++ src/download.rs
@@ -1,6 +1,15 @@
 use crate::package::配方包;
 
+use git2::Repository;
+use std::path::PathBuf;
+
 pub fn 下載配方包(包: 配方包) {
     log::debug!("下載配方包: {}, 位於 {}", 包.配方, 包.倉庫);
     todo!("還沒做呢")
+}
+
+fn 搬運倉庫(包: &配方包, 本地路徑: &PathBuf) -> anyhow::Result<()> {
+    let 網址 = &包.倉庫.網址;
+    let 倉庫 = Repository::clone(網址, 本地路徑)?;
+    Ok(())
 }
