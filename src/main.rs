debfddb5c6579d6d8bb14cd66e5a6909fdfe0fec
fxliang <fx.liang@outlook.com>
2025-03-04 12:31:00 +0800

    feat: set proxy with option `--proxy <proxy>`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+use regex::Regex;
 
 mod download;
 mod install;
@@ -17,6 +18,16 @@ use rime_levers::{
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Rime 配方管理器")]
+struct 命令行參數 {
+    // 代理 URL 作爲選項
+    #[structopt(short,long = "proxy", global = true, help = "代理服務器地址")]
+    proxy: Option<String>,
+    // 子命令
+    #[structopt(subcommand)]
+    子命令: 子命令,
+}
+
+#[derive(Debug, StructOpt)]
 enum 子命令 {
     /// 加入輸入方案列表
     Add {
@@ -61,9 +72,20 @@ enum 子命令 {
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let 命令行參數 = 子命令::from_args();
+    let 命令行參數 = 命令行參數::from_args();
     log::debug!("參數: {:?}", 命令行參數);
-    match 命令行參數 {
+    if let Some(代理地址) = 命令行參數.proxy {
+        let 代理規則 = Regex::new(r"^(http://|https://)?[a-zA-Z0-9.-]+(:\d+)?$").unwrap();
+        if 代理規則.is_match(&代理地址) {
+            std::env::set_var("http_proxy", 代理地址.as_str());
+            std::env::set_var("https_proxy", 代理地址.as_str());
+            log::debug!("設置代理 {}", 代理地址);
+        } else {
+            eprintln!("無效的代理地址: {} ", 代理地址);
+            std::process::exit(1);
+        }
+    }
+    match 命令行參數.子命令 {
         子命令::Add { schemata } => {
             let 還不知道怎麼傳過來 = PathBuf::from(".");
             設置引擎啓動參數(&還不知道怎麼傳過來)?;
